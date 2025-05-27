dfd03deffd4fe4c5e9106842d12c142f837f3820
Shubhi <shubhig389@gmail.com>
2025-05-27 11:56:35 +0530

    updated Solution2.java

--- Day02_ValidAnagram/Solution2.java
+++ Day02_ValidAnagram/Solution2.java
@@ -1,33 +1,32 @@
 public class Solution2 {
     
-
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        boolean result = isAnagram(s, t);
+        System.out.println("Is \"" + s + "\" an anagram of \"" + t + "\": " + result);
+    }
+    
+    public static boolean isAnagram(String s, String t){
+        if(s.length()!=t.length()) return false;
+        
+        int[] frequency=new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            frequency[ch-'a']++;
         }
-
-        int[] charCount = new int[26]; // Assuming only lowercase letters a-z
-
-        for (int i = 0; i < s.length(); i++) {
-            charCount[s.charAt(i) - 'a']++;
-            charCount[t.charAt(i) - 'a']--;
+        
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            frequency[ch-'a']--;
         }
-
-        for (int count : charCount) {
-            if (count != 0) {
+        
+        for(int i=0;i<26;i++){
+            if(frequency[i]!=0){
                 return false;
             }
         }
-
         return true;
-    }
-
-
-    public static void main(String[] args){
-        Solution2 solution = new Solution2();
-        String s = "anagram";
-        String t = "nagaram";
-        boolean result = solution.isAnagram(s, t);
-        System.out.println("Is \"" + s + "\" an anagram of \"" + t + "\": " + result);
     }
 }
