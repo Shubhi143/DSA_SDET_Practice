40aa0ec31138cb3afa2142e957b61f3133f3ac88
Shubhi <shubhig389@gmail.com>
2025-05-28 12:23:24 +0530

    find duplicate number

--- Day12_FindDuplicateNumber/README.md
+++ Day12_FindDuplicateNumber/README.md
@@ -1,6 +1,21 @@
 # Day12 FindDuplicateNumber
 
-- [ ] Problem link: 
+- [ ] Problem link: https://leetcode.com/problems/find-the-duplicate-number/description/
+- [ ] Problem description: There is a array of integers. Some integers may repeat and need to
+be removed. We need to return the number of unique integers in the array.
+- [ ] Example 1:
+- Input: nums = [1,2,2,3,4,4,5]
+- Output: 5
+- Explanation: The unique integers in the array are [1,2,3,4,5
+- ] 
 - [ ] Approach:
+- [ ] Use a set to store unique integers
+- [ ] Iterate through the array and add each integer to the set
+- [ ] Return the size of the set
+
 - [ ] Code:
+Solution12.java
+
 - [ ] Time & Space Complexity:
+- [ ] Time complexity: O(n)
+- [ ] Space complexity: O(n)
---
+++ Day12_FindDuplicateNumber/Solution12.java
@@ -0,0 +1,28 @@
+package Day12_FindDuplicateNumber;
+
+public class Solution12 {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 4, 2, 2};
+        int result = findDuplicate(nums);
+        System.out.println("The duplicate number is: " + result);
+    }
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Finding the intersection point
+        do {
+            slow = nums[slow]; // Move slow pointer by 1 step
+            fast = nums[nums[fast]]; // Move fast pointer by 2 steps
+        } while (slow != fast);
+
+        // Phase 2: Finding the entrance to the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow; // The duplicate number
+    }
+}
